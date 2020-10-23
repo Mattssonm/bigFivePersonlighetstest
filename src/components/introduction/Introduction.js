2db0d71fc2b809046e9d2402516ce869413eb6b6
Mattssonm <markusmattsson90@gmail.com>
2020-10-23 07:32:16 +0200

    Add media queries, general styles and intro text

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -6,7 +6,9 @@ import Button from '../general/Button'
 export default function Introduction() {
 	return (
 		<div className="instructions">
-			<h3>Instruktioner</h3>
+			<h1>Big Five Personlighetstest</h1>
+      <p>Big Five teorin är den personlighetsteori som har störst vetenskaplig grund inom personlighetspsykologin. Gör testet nedan för att få reda på din rankning i de fem personlighetsdragen. </p>
+      <h3>Instruktioner</h3>
 			<Introtext />
       <div className="flex-center">
         <Button text={"Starta testet"} />
