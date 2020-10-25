1c5bac5b7d40fded8feb8bc1078de94dfa49ca29
Mattssonm <markusmattsson90@gmail.com>
2020-10-25 09:20:04 +0100

    Updates

---
+++ precache-manifest.f2c8481f3ec14c888b1c69090cee6008.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d533d4e5a266a6770c85f2c6bfb45d5d",
+    "url": "/bigfivepersonlighetstest/index.html"
+  },
+  {
+    "revision": "ea0c046395c30a0ef389",
+    "url": "/bigfivepersonlighetstest/static/css/main.baf2465c.chunk.css"
+  },
+  {
+    "revision": "33db1a433c76f52a57f2",
+    "url": "/bigfivepersonlighetstest/static/js/2.63833b1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bigfivepersonlighetstest/static/js/2.63833b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea0c046395c30a0ef389",
+    "url": "/bigfivepersonlighetstest/static/js/main.4b1d49ff.chunk.js"
+  },
+  {
+    "revision": "755dbf2b020e01678da8",
+    "url": "/bigfivepersonlighetstest/static/js/runtime-main.5cc4258d.js"
+  }
+]);
